1d3ed276103e2ce04f08cb5a8ae69d6e3a9e1589
rachel <rachelderp@gmail.com>
2012-03-14 08:34:18 -0600

    It works! Removed debug messages and rearranged our $reconnect

--- plugins/reconnect.pl
+++ plugins/reconnect.pl
@@ -10,32 +10,24 @@ use Network;
 use Globals;
 
 
-our $reconnect;
+our $reconnect ||= {
+					'timeout' => [30,	# 30 seconds
+								  60,	# 1 minute
+								  60,	# 1 minute
+								  180,	# 3 minutes
+								  180,	# 3 minutes
+								  300,	# 5 minutes
+								  300,	# 5 minutes
+								  900, 	# 15 minutes
+								  900, 	# 15 minutes
+								  1800,	# 30 minutes
+								  3600],# 1 hour
+					
+					'random'	=> 30,
+					'counter'	=> 0
+					};
 
-# We need to check if these variables haven't been defined yet.
-# Otherwise Kore will overwrite them if the plugin is ever reloaded.
-if(ref($reconnect) ne 'HASH')
-{
-	$reconnect = {};
-	
-	$reconnect->{timeout} = [30,	# 30 seconds
-							 60,	# 1 minute
-							 60,	# 1 minute
-							 180,	# 3 minutes
-							 180,	# 3 minutes
-							 300,	# 5 minutes
-							 300,	# 5 minutes
-							 900, 	# 15 minutes
-							 900, 	# 15 minutes
-							 1800,	# 30 minutes
-							 3600];	# 1 hour
-
-
-	$reconnect->{random} = 30;
-	$reconnect->{counter} = 0;	
-}
-
-Plugins::register("Reconnect", "Version 0.1 r5", \&unload);
+Plugins::register("Reconnect", "Version 0.1 r6", \&unload);
 my $hooks = Plugins::addHooks(['mainLoop_post', \&loop],
 								['packet/received_character_ID_and_Map', \&connected]);
 
@@ -51,17 +43,14 @@ sub loop
 	
 	if(Network::DirectConnection::getState() == Network::NOT_CONNECTED and $reconnect->{time} < $time)
 	{
-		print("NOT CONNECTED?!?!?!\n");
 		my $reconnectTime = @{$reconnect->{timeout}}[$reconnect->{counter}];
 
 		if($reconnect->{random}) {
 			$reconnectTime += int(rand($reconnect->{random}));
 		}
 
-		print("RECONNECT TIME SET TO $timeout{reconnect}->{timeout} !!!!!!!!!!!!!!!!!!!!!!\n");
 		$reconnect->{time} = $time + $timeout{reconnect}->{timeout};
 		$timeout{reconnect} = {'timeout' => $reconnectTime};
-		print("SET TIMEOUT TO $reconnectTime !!!!!!!!!!!!!!!!!!!!!!!!!\n");
 		
 		my $sizeOf = @{$reconnect->{timeout}};	
 		if($reconnect->{counter} < $sizeOf - 1) {
@@ -73,7 +62,6 @@ sub loop
 sub connected
 {
 	my $time = time();	
-	print("COUNTER RESET!!!!!!!!!!!!!!!!!!!!!!!!!!1\n");
 	$reconnect->{counter} = 0;
 	
 	my $reconnectTime = @{$reconnect->{timeout}}[$reconnect->{counter}];
@@ -83,7 +71,6 @@ sub connected
 	}
 
 	$timeout{reconnect} = {'timeout' => $reconnectTime};
-	print("SET TIMEOUT TO $reconnectTime !!!!!!!!!!!!!!!!!!!!!!!!!\n");
 }
 
 1;
